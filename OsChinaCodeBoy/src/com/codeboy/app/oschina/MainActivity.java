1caf02b43c0e591555f8776a3c6683a1417303f9
LeonLee <lendylongli@gmail.com>
2014-04-24 00:24:39 +0800

    主界面加入TabHost+ViewPager

--- OsChinaCodeBoy/src/com/codeboy/app/oschina/MainActivity.java
+++ OsChinaCodeBoy/src/com/codeboy/app/oschina/MainActivity.java
@@ -1,24 +1,58 @@
 package com.codeboy.app.oschina;
 
-import com.codeboy.app.oschina.ui.NewsLatestNewsFragment;
+import java.util.ArrayList;
 
+import com.codeboy.app.oschina.ui.NewsLatestNewsFragment;
+import com.codeboy.app.oschina.ui.NewsRecentBlogPostsFragment;
+import com.codeboy.app.oschina.ui.NewsTweetFragment;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TabHost;
+import android.widget.TabWidget;
 
 public class MainActivity extends BaseActionBarActivity {
+	
+	TabHost mTabHost;
+    ViewPager  mViewPager;
+    TabsAdapter mTabsAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		if(savedInstanceState == null) {
+		/*if(savedInstanceState == null) {
 			getSupportFragmentManager()
 			.beginTransaction()
 			.replace(R.id.container, NewsLatestNewsFragment.newInstance())
 			.commit();
-		}
+		}*/
+		mTabHost = (TabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup();
+
+        mViewPager = (ViewPager)findViewById(R.id.viewpager);
+
+        mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
+
+        mTabsAdapter.addTab(mTabHost.newTabSpec("news").setIndicator("最新资讯"),
+        		NewsLatestNewsFragment.class, null);
+        mTabsAdapter.addTab(mTabHost.newTabSpec("blog").setIndicator("最新博客"),
+                NewsRecentBlogPostsFragment.class, null);
+        mTabsAdapter.addTab(mTabHost.newTabSpec("tweet").setIndicator("推荐阅读"),
+                NewsTweetFragment.class, null);
+
+        if (savedInstanceState != null) {
+            mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
+        }
 	}
 
 	@Override
@@ -42,5 +76,98 @@ public class MainActivity extends BaseActionBarActivity {
 			return true;
 		} 
 		return super.onOptionsItemSelected(item);
+	}
+	
+	static class TabsAdapter extends FragmentPagerAdapter 
+		implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
+
+		private final Context mContext;
+        private final TabHost mTabHost;
+        private final ViewPager mViewPager;
+        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+
+        static final class TabInfo {
+            private final String tag;
+            private final Class<?> clss;
+            private final Bundle args;
+
+            TabInfo(String _tag, Class<?> _class, Bundle _args) {
+                tag = _tag;
+                clss = _class;
+                args = _args;
+            }
+        }
+
+        static class DummyTabFactory implements TabHost.TabContentFactory {
+            private final Context mContext;
+
+            public DummyTabFactory(Context context) {
+                mContext = context;
+            }
+
+            @Override
+            public View createTabContent(String tag) {
+                View v = new View(mContext);
+                v.setMinimumWidth(0);
+                v.setMinimumHeight(0);
+                return v;
+            }
+        }
+
+        public TabsAdapter(FragmentActivity activity, TabHost tabHost, ViewPager pager) {
+            super(activity.getSupportFragmentManager());
+            mContext = activity;
+            mTabHost = tabHost;
+            mViewPager = pager;
+            mTabHost.setOnTabChangedListener(this);
+            mViewPager.setAdapter(this);
+            mViewPager.setOnPageChangeListener(this);
+        }
+        
+        public void addTab(TabHost.TabSpec tabSpec, Class<?> clss, Bundle args) {
+            tabSpec.setContent(new DummyTabFactory(mContext));
+            String tag = tabSpec.getTag();
+
+            TabInfo info = new TabInfo(tag, clss, args);
+            mTabs.add(info);
+            mTabHost.addTab(tabSpec);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return mTabs.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            TabInfo info = mTabs.get(position);
+            return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+        }
+
+        @Override
+        public void onTabChanged(String tabId) {
+            int position = mTabHost.getCurrentTab();
+            mViewPager.setCurrentItem(position);
+        }
+
+		@Override
+		public void onPageScrollStateChanged(int state) {
+			
+		}
+
+		@Override
+		public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+			
+		}
+
+		@Override
+        public void onPageSelected(int position) {
+			TabWidget widget = mTabHost.getTabWidget();
+            int oldFocusability = widget.getDescendantFocusability();
+            widget.setDescendantFocusability(ViewGroup.FOCUS_BLOCK_DESCENDANTS);
+            mTabHost.setCurrentTab(position);
+            widget.setDescendantFocusability(oldFocusability);
+		}
 	}
 }
